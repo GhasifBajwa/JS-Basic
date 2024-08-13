0db36fb5b831b69c08168383233174070d9d5ada
GhasifBajwa <155342512+GhasifBajwa@users.noreply.github.com>
2024-08-13 11:34:17 +0000

    Datatypes discussion

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using node js not browser
+
+console.log(3+3); //code readabiity should be high
+console.log("ghasif");
+
+let name = "ghasif";
+let age = 19;
+let logIn = false;
+
+//all datatypes are given below
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean=> true/false
+//null => standalone vlue
+//undefined => when value is not given
+//symbol => unique
+
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
