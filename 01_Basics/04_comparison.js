9bd817748c45189c1eb62d536f4e29c192ac9ee9
GhasifBajwa <155342512+GhasifBajwa@users.noreply.github.com>
2024-08-17 08:36:53 +0000

    comparisons

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,23 @@
+//basic operations
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 === 1);
+console.log(2 !== 1);
+// basic operations with string or other data types
+console.log("2" > 1);
+console.log("02" > 1);
+// null and undefined give diiferent results as comapared to expectation
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+// === strict equality operator
+console.log(2 === "2");
