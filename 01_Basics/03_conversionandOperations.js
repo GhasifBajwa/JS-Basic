aac7ff74e0ff7a43e0cce427f63e57a7d06ef0cc
GhasifBajwa <155342512+GhasifBajwa@users.noreply.github.com>
2024-08-17 08:18:51 +0000

    operations

--- 01_Basics/03_conversionandOperations.js
+++ 01_Basics/03_conversionandOperations.js
@@ -15,9 +15,9 @@ console.log(valueInNumber);
 //true=>1; false=>0
 
 let logIn = 1; //true
-let logIn1 = 0;//false
-let logIn2 = "";//false
-let logIn3 = "ghasif";//true
+let logIn1 = 0; //false
+let logIn2 = ""; //false
+let logIn3 = "ghasif"; //true
 let boolLogIn = Boolean(logIn);
 console.log(boolLogIn);
 
@@ -29,3 +29,40 @@ let someNumer = 33;
 let stringNumber = String(someNumer);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//Operaions
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(2 / 3);
+console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = "=ghasif";
+let str3 = str1 + str2;
+console.log(str3);
+
+//bad paracice
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+//good paractice
+console.log(((3 + 4) * 5) % 8);
+
+//more paractice + example according to good or bad paractices
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
