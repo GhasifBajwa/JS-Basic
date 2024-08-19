d268d78ddb45aeba96a784d5bfd61d419c6e3b3b
GhasifBajwa <155342512+GhasifBajwa@users.noreply.github.com>
2024-08-19 09:23:55 +0000

    objects destructure

---
+++ 02_Basics/05_Objects_deStructure.js
@@ -0,0 +1,23 @@
+// Objects De_Structure
+
+const course = {
+  coursename: "Javascript",
+  price: 500,
+  courseInstructor: "Ghasif",
+};
+
+const { coursename, price, courseInstructor } = course;
+console.log(courseInstructor);
+
+
+// API and JSON format
+// {
+//     'name': "Ghasif",
+//     "coursename": "Javascript",
+//     "price": 500,
+// }
+// [
+//     {},
+//     {},
+//     {},
+// ]
