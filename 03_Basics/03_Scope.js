91f7495d581549a70b1f9a07a6746002894c4954
GhasifBajwa <155342512+GhasifBajwa@users.noreply.github.com>
2024-08-20 05:36:33 +0000

    Scope

---
+++ 03_Basics/03_Scope.js
@@ -0,0 +1,14 @@
+let num1 = 100;
+const num2 = 200;
+
+if (true) {
+  let num1 = 10;
+  const num2 = 20;
+  console.log("Inner:",num1);
+  console.log("Inner:",num2);
+//   var num3 = 30;
+}
+
+console.log(num1);
+console.log(num2);
+// console.log(num3);
