f9b6a4ccc7616800491e6c57a32b5f535abfb083
GhasifBajwa <155342512+GhasifBajwa@users.noreply.github.com>
2024-09-19 17:34:20 +0000

    For loop added in repo

--- 05_Loops/5_loops.js
+++ 05_Loops/5_loops.js
@@ -1,1 +1,40 @@
 // for each loop
+
+const coding = ["HTML", "CSS", "Javascript", "PHP", "Python"];
+// coding.forEach(function (element) {
+//   console.log(element);
+// });
+coding.forEach((element) => {
+  console.log(element);
+});
+
+coding.forEach((item, index, arr) => {
+  console.log(item, index, arr);
+});
+
+const myCoding = [
+  {
+    name: "HTML",
+    level: "Intermediate",
+  },
+  {
+    name: "CSS",
+    level: "Intermediate",
+  },
+  {
+    name: "Javascript",
+    level: "Beginner",
+  },
+  {
+    name: "PHP",
+    level: "Beginner",
+  },
+  {
+    name: "Python",
+    level: "Beginner",
+  },
+];
+myCoding.forEach((item) => {
+  console.log(item.name);
+  console.log(item.level);
+})
